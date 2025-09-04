744757d31d749bac3f26572d1880c3ee5201269e
Kyle-Galloway01 <123207266+Kyle-Galloway01@users.noreply.github.com>
2025-09-04 05:12:17 -0500

    backend: add Pydantic schemas (DTOs)

---
+++ backend/app/schemas.py
@@ -0,0 +1,27 @@
+﻿from pydantic import BaseModel, EmailStr
+
+class CustomerCreate(BaseModel):
+    name: str
+    email: EmailStr
+
+class CustomerOut(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    class Config:
+        from_attributes = True
+
+class TransactionOut(BaseModel):
+    id: int
+    amount: float
+    ts: str
+    class Config:
+        from_attributes = True
+
+class LoginIn(BaseModel):
+    username: str
+    password: str
+
+class TokenOut(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
